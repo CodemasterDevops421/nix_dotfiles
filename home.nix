f95943c346b8a53bd3b67ee98ac3d2409ee0e843
CodemasterDevops421 <melamchaitanyakumar@gmail.com>
2025-05-06 01:34:28 +0000

    example commot from main

--- home.nix
+++ home.nix
@@ -32,7 +32,7 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
-
+    #this is the change 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
