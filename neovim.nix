e4be1fd05de9a9ceada1490e1092cc852fd3c1a1
CodemasterDevops421 <melamchaitanyakumar@gmail.com>
2025-05-06 00:26:09 +0000

    Adding neovim plugins : lazygit integration

--- neovim.nix
+++ neovim.nix
@@ -1,8 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  programs.neovim.enable = true;
-  programs.neovim.viAlias = true;
-  programs.neovim.vimAlias = true;
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
 
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      telescope-nvim
+      lazygit-nvim
+      nvim-treesitter
+      # Add more plugins here as needed
+    ];
+
+    extraConfig = ''
+      set number
+      syntax on
+    '';
+  };
 }
