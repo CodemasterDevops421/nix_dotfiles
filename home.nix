077a1dc27bc7d7f01013a1423f3d5e8c23d838db
CodemasterDevops421 <melamchaitanyakumar.com>
2025-05-07 22:31:38 +0000

    updating the terminal fonts and configs in home.nix

--- home.nix
+++ home.nix
@@ -1,5 +1,4 @@
 { config, pkgs, lib, ... }:
-
 {
   ##################################################
   # Basic user info
@@ -7,7 +6,6 @@
   home.username      = "chaithu";
   home.homeDirectory = "/home/chaithu";
   home.stateVersion  = "24.11";
-
   ##################################################
   # Packages (apps + fonts)
   ##################################################
@@ -19,7 +17,6 @@
     nerd-fonts.jetbrains-mono
     nerd-fonts.hack
   ];
-
   ##################################################
   # Z-shell configuration
   ##################################################
@@ -28,13 +25,11 @@
     enableCompletion          = true;
     autosuggestion.enable     = true;
     syntaxHighlighting.enable = true;
-
     oh-my-zsh = {
       enable  = true;
       theme   = "agnoster";
       plugins = [ "git" "kubectl" "terraform" ];
     };
-
     shellAliases = {
       ll   = "eza -alh --icons";
       gs   = "git status";
@@ -43,46 +38,74 @@
       gp   = "git push";
       nixr = "sudo nixos-rebuild switch --flake .#chaithu";
     };
-
     initContent = ''
       # histdb plugin
       fpath=(${pkgs.zsh-histdb}/share/zsh/site-functions $fpath)
       autoload -Uz histdb histdb-top _histdb-isearch
       bindkey '^R' _histdb-isearch
-
-      # Starship prompt
-      eval "$(${pkgs.starship}/bin/starship init zsh)"
+      # Starship prompt integration
+      eval "$(starship init zsh)"
+      # Optional: direnv hook
+      if command -v direnv >/dev/null; then eval "$(direnv hook zsh)"; fi
     '';
   };
-
   ##################################################
   # Starship prompt
   ##################################################
   programs.starship = {
-    enable   = true;
+    enable = true;
     settings = {
-      add_newline = false;
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol   = "[✗](bold red)";
+      add_newline = true;
+      
+      # Single line version with angular segments
+      format = "$username$directory$git_branch$cmd_duration$time$character";
+      
+      username = {
+        style_user = "bg:yellow fg:black";
+        style_root = "bg:red fg:black";
+        format = "[$user]($style)";
+        show_always = true;
       };
-      git_branch = { symbol = "🌱 "; };
-      kubernetes = { symbol = "⛵ "; disabled = false; };
-      terraform  = { symbol = "💠 "; };
-      aws        = { symbol = "☁️  "; };
+      
+      directory = {
+        truncation_length = 3;
+        style = "bg:blue fg:black";
+        format = "[ $path ]($style)";
+      };
+      
+      git_branch = {
+        style = "bg:green fg:black";
+        format = "[ $symbol$branch ]($style)";
+        symbol = "󰊢 ";
+      };
+      
+      cmd_duration = {
+        style = "bg:cyan fg:black";
+        format = "[ 󱦟 $duration ]($style)";
+        min_time = 500;
+      };
+      
+      time = {
+        disabled = false;
+        style = "bg:purple fg:black";
+        format = "[ 󰥔 $time ]($style)";
+        time_format = "%H:%M:%S";
+      };
+      
+      character = {
+        success_symbol = "[➜ ](bold green)";
+        error_symbol = "[✗ ](bold red)";
+      };
     };
   };
-
   ##################################################
   # Fonts & session variables
   ##################################################
   fonts.fontconfig.enable = true;
-
   home.sessionVariables = {
     EDITOR = "vim";
     PAGER  = "less";
   };
-
   ##################################################
   # Home-Manager self-management
   ##################################################
