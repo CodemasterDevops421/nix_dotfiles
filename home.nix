83365c1a6144e82f37c1e44ede0f4b14bd62c9f1
CodemasterDevops421 <melamchaitanyakumar.com>
2025-05-07 20:10:46 +0000

    fix home.nix

--- home.nix
+++ home.nix
@@ -1,28 +1,28 @@
 { config, pkgs, lib, ... }:
 
 {
-  ########################################
-  # User & state
-  ########################################
+  ##################################################
+  # Basic user info
+  ##################################################
   home.username      = "chaithu";
   home.homeDirectory = "/home/chaithu";
   home.stateVersion  = "24.11";
 
-  ########################################
-  # Packages  (fonts moved here)
-  ########################################
+  ##################################################
+  # Packages (apps + fonts)
+  ##################################################
   home.packages = with pkgs; [
-    neofetch  xclip   tmux    tree   zsh   dos2unix  ghostty  alacritty
-    bat       fzf     eza     ripgrep zoxide bottom  starship sqlite
-
-    nerd-fonts.hack
+    neofetch xclip tmux tree zsh dos2unix ghostty alacritty
+    bat fzf eza ripgrep zoxide bottom starship sqlite
+    zsh-histdb
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
+    nerd-fonts.hack
   ];
 
-  ########################################
-  # Z-shell
-  ########################################
+  ##################################################
+  # Z-shell configuration
+  ##################################################
   programs.zsh = {
     enable                    = true;
     autosuggestion.enable     = true;
@@ -30,7 +30,7 @@
 
     oh-my-zsh = {
       enable  = true;
-      theme   = "";
+      theme   = "agnoster";
       plugins = [ "git" "docker" "kubectl" "terraform" ];
     };
 
@@ -44,31 +44,37 @@
     };
 
     initContent = ''
+      # histdb plugin
+      fpath=(${pkgs.zsh-histdb}/share/zsh/site-functions $fpath)
+      autoload -Uz histdb histdb-top _histdb-isearch
+      bindkey '^R' _histdb-isearch
+
       # Starship prompt
       eval "$(${pkgs.starship}/bin/starship init zsh)"
-      # Better reverse-i-search
-      bindkey '^R' history-incremental-search-backward
     '';
   };
 
-  ########################################
+  ##################################################
   # Starship prompt
-  ########################################
+  ##################################################
   programs.starship = {
-    enable = true;
+    enable   = true;
     settings = {
       add_newline = false;
-      character  = { success_symbol = "[➜](bold green)"; error_symbol = "[✗](bold red)"; };
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol   = "[✗](bold red)";
+      };
       git_branch = { symbol = "🌱 "; };
       kubernetes = { symbol = "⛵ "; disabled = false; };
       terraform  = { symbol = "💠 "; };
-      aws        = { symbol = "☁️ "; };
+      aws        = { symbol = "☁️  "; };
     };
   };
 
-  ########################################
-  # Fonts & env
-  ########################################
+  ##################################################
+  # Fonts & session variables
+  ##################################################
   fonts.fontconfig.enable = true;
 
   home.sessionVariables = {
@@ -76,24 +82,8 @@
     PAGER  = "less";
   };
 
-  ########################################
-  # One-time fixups on every switch
-  ########################################
-  home.activation = {
-    fixCacheDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ -d "$HOME/.cache/oh-my-zsh" ]; then
-        chown -R "$USER:$USER" "$HOME/.cache/oh-my-zsh" || true
-      fi
-    '';
-    stripCRLF = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
-      if command -v dos2unix >/dev/null 2>&1; then
-        dos2unix "$HOME/.zshrc" >/dev/null 2>&1 || true
-      fi
-    '';
-  };
-
-  ########################################
+  ##################################################
   # Home-Manager self-management
-  ########################################
+  ##################################################
   programs.home-manager.enable = true;
 }
