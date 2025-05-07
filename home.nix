cbd6257c4441f2f7e6bfaaf1f5ec51df89c5f678
CodemasterDevops421 <melamchaitanyakumar@gmail.com>
2025-05-07 19:37:02 +0000

    reverting the updated files for home.nix

--- home.nix
+++ home.nix
@@ -1,27 +1,28 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  ##################################################
-  # Basic user info
-  ##################################################
+  ########################################
+  # User & state
+  ########################################
   home.username      = "chaithu";
   home.homeDirectory = "/home/chaithu";
   home.stateVersion  = "24.11";
 
-  ##################################################
-  # Packages
-  ##################################################
+  ########################################
+  # Packages  (fonts moved here)
+  ########################################
   home.packages = with pkgs; [
     neofetch  xclip   tmux    tree   zsh   dos2unix  ghostty  alacritty
     bat       fzf     eza     ripgrep zoxide bottom  starship sqlite
-    zsh-histdb                             # history‑to‑SQLite plugin
+
+    nerd-fonts.hack
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
   ];
 
-  ##################################################
-  # Zsh Configuration
-  ##################################################
+  ########################################
+  # Z-shell
+  ########################################
   programs.zsh = {
     enable                    = true;
     autosuggestion.enable     = true;
@@ -29,7 +30,7 @@
 
     oh-my-zsh = {
       enable  = true;
-      theme   = "agnoster";
+      theme   = "";
       plugins = [ "git" "docker" "kubectl" "terraform" ];
     };
 
@@ -42,38 +43,32 @@
       nixr = "sudo nixos-rebuild switch --flake .#chaithu";
     };
 
-    initExtra = ''
-      # histdb plugin
-      fpath=(${pkgs.zsh-histdb}/share/zsh/site-functions $fpath)
-      autoload -Uz histdb histdb-top _histdb-isearch
-      histdb-install || true
-      bindkey '^R' _histdb-isearch
+    initContent = ''
+      # Starship prompt
+      eval "$(${pkgs.starship}/bin/starship init zsh)"
+      # Better reverse-i-search
+      bindkey '^R' history-incremental-search-backward
     '';
   };
 
-  ##################################################
-  # Starship Prompt
-  ##################################################
+  ########################################
+  # Starship prompt
+  ########################################
   programs.starship = {
-    enable   = true;
+    enable = true;
     settings = {
       add_newline = false;
-
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol   = "[✗](bold red)";
-      };
-
+      character  = { success_symbol = "[➜](bold green)"; error_symbol = "[✗](bold red)"; };
       git_branch = { symbol = "🌱 "; };
       kubernetes = { symbol = "⛵ "; disabled = false; };
       terraform  = { symbol = "💠 "; };
-      aws        = { symbol = "☁️  "; };
+      aws        = { symbol = "☁️ "; };
     };
   };
 
-  ##################################################
-  # Fonts & Session Variables
-  ##################################################
+  ########################################
+  # Fonts & env
+  ########################################
   fonts.fontconfig.enable = true;
 
   home.sessionVariables = {
@@ -81,8 +76,24 @@
     PAGER  = "less";
   };
 
-  ##################################################
-  # Home-Manager Self-Management
-  ##################################################
+  ########################################
+  # One-time fixups on every switch
+  ########################################
+  home.activation = {
+    fixCacheDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ -d "$HOME/.cache/oh-my-zsh" ]; then
+        chown -R "$USER:$USER" "$HOME/.cache/oh-my-zsh" || true
+      fi
+    '';
+    stripCRLF = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      if command -v dos2unix >/dev/null 2>&1; then
+        dos2unix "$HOME/.zshrc" >/dev/null 2>&1 || true
+      fi
+    '';
+  };
+
+  ########################################
+  # Home-Manager self-management
+  ########################################
   programs.home-manager.enable = true;
 }
