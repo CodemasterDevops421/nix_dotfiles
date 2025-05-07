287cf63ba742d11def9dde3469fdc5f19792e44e
CodemasterDevops421 <melamchaitanyakumar.com>
2025-05-07 20:13:41 +0000

    fix home.nix

--- home.nix
+++ home.nix
@@ -25,13 +25,14 @@
   ##################################################
   programs.zsh = {
     enable                    = true;
+    enableCompletion          = true;
     autosuggestion.enable     = true;
     syntaxHighlighting.enable = true;
 
     oh-my-zsh = {
       enable  = true;
       theme   = "agnoster";
-      plugins = [ "git" "docker" "kubectl" "terraform" ];
+      plugins = [ "git" "kubectl" "terraform" ];
     };
 
     shellAliases = {
